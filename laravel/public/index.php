cbea48b160d5e3420e812daa5273d453f1c2858a
Yanwardo Sitanggang <ywdfsctgclev0301@gmail.com>
2020-02-27 09:13:39 +0700

    Index, penerapan bootstrap

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -1,5 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+        <meta charset="utf-8">
+        <title>Beranda CRUD</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://localhost/pabw/bootstrap/css/bootstrap.min.css">
+        
+        <!-- jQuery library -->
+        <script src="http://localhost/pabw/bootstrap/js/jquery.min.js"></script>
+        
+        <!-- Latest compiled JavaScript -->
+        <script src="http://localhost/pabw/bootstrap/js/bootstrap.min.js"></script>
 
-echo "<h1>Oke ini adalah dari rapidcorlearn lewat git</h1>";
-
-?>
+    </head>
+    <body>
+        <div class="container">
+            <h1>The Owl Team</h1>
+        </div>
+    </body>
+</html>
